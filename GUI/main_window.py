ce5a443d60f712412c45c8cad5cb2ae9e65d4ecc
Jekov-Evgen <jena1245481te@gmail.com>
2024-11-25 00:13:22 +0200

    result app

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -1,7 +1,8 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit
+from PyQt6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit, QMessageBox
 from GUI.style.style_Qt import CONST_MAIN_WINDOW
+from Logics.generation import generate
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
@@ -10,6 +11,8 @@ class MainWindow(QMainWindow):
         self.setFixedSize(400, 300)
         self.setStyleSheet(CONST_MAIN_WINDOW)
         self.setWindowIcon(QIcon(r"image\icon.webp"))
+        
+        self.success = True
 
         control_UI = QVBoxLayout()
         central_widget = QWidget()
@@ -17,6 +20,7 @@ class MainWindow(QMainWindow):
         instructions = QLabel(text="Введите url и мы создадим для вас QR_CODE")
         self.enter = QLineEdit()
         generator = QPushButton(text="Начать генерацию")
+        generator.clicked.connect(self.start)
         
         control_UI.addWidget(instructions, alignment=Qt.AlignmentFlag.AlignCenter)
         control_UI.addWidget(self.enter)
@@ -26,3 +30,22 @@ class MainWindow(QMainWindow):
         
         self.setCentralWidget(central_widget)
         self.show()
+        
+    def start(self):
+        try:
+            url = self.enter.text()
+            generate(url)
+        except:
+            self.success = False
+            error = QMessageBox()
+            error.setWindowIcon(QIcon(r"image\icon.webp"))
+            error.setWindowTitle("Ошибка")
+            error.setText("Ошибка, скорее всего проблема в url")
+            error.exec()
+            
+        if self.success == True:
+            result = QMessageBox()
+            result.setWindowIcon(QIcon(r"image\icon.webp"))
+            result.setWindowTitle("Успех")
+            result.setText("На рабочем столе создан url")
+            result.exec()
