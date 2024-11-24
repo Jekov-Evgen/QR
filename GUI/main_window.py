42298d8e2d6922faa39dff192fddd071c2a683f8
Jekov-Evgen <jena1245481te@gmail.com>
2024-11-25 00:06:54 +0200

    result

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -1,11 +1,28 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QMainWindow, QWidget
+from PyQt6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit
+from GUI.style.style_Qt import CONST_MAIN_WINDOW
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setWindowTitle("генератор QR")
+        self.setFixedSize(400, 300)
+        self.setStyleSheet(CONST_MAIN_WINDOW)
+        self.setWindowIcon(QIcon(r"image\icon.webp"))
 
+        control_UI = QVBoxLayout()
+        central_widget = QWidget()
         
+        instructions = QLabel(text="Введите url и мы создадим для вас QR_CODE")
+        self.enter = QLineEdit()
+        generator = QPushButton(text="Начать генерацию")
+        
+        control_UI.addWidget(instructions, alignment=Qt.AlignmentFlag.AlignCenter)
+        control_UI.addWidget(self.enter)
+        control_UI.addWidget(generator)
+        
+        central_widget.setLayout(control_UI) 
+        
+        self.setCentralWidget(central_widget)
         self.show()
---
+++ GUI/style/style_Qt.py
@@ -0,0 +1,42 @@
+CONST_MAIN_WINDOW = """
+QMainWindow {
+        background-color: #f0f0f0;
+    }
+    
+    QLabel {
+        font-size: 16px;
+        font-weight: bold;
+        color: #333333;
+    }
+    
+    QLineEdit {
+        border: 2px solid #4CAF50;
+        border-radius: 5px;
+        padding: 5px;
+        font-size: 14px;
+        color: #333333;
+        background-color: #ffffff;
+    }
+    
+    QLineEdit:focus {
+        border-color: #45a049;
+    }
+    
+    QPushButton {
+        background-color: #4CAF50;
+        border: none;
+        border-radius: 5px;
+        padding: 10px;
+        font-size: 14px;
+        color: white;
+        font-weight: bold;
+    }
+    
+    QPushButton:hover {
+        background-color: #45a049;
+    }
+    
+    QPushButton:pressed {
+        background-color: #3e8e41;
+    }
+"""
